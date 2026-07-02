0e976bf7422e9747b2f5c340108c06dde4efdbbd
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-07-02 22:21:03 +0530

    added pokemon api endpoint

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    //fetch pokemon
+    fetchPokemons();
+  }, []);
+  const fetchPokemons = async () => {
+    try {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+      const data = await res.json();
+      setPokemons(data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
